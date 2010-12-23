d900d579295dd9a941a1e1832445c7a65e76d420
Akkorian-257529 <am@armord.net>
2010-12-23 05:12:14 +0000

    Fixed the background for panels when an addon is loaded that changes the GameTooltip background

--- PhanxConfig-Panel.lua
+++ PhanxConfig-Panel.lua
@@ -12,7 +12,11 @@ local MINOR_VERSION = tonumber(("$Revision$"):match("%d+"))
 local lib, oldminor = LibStub:NewLibrary("PhanxConfig-Panel", MINOR_VERSION)
 if not lib then return end
 
-local panelBackdrop = GameTooltip:GetBackdrop()
+local panelBackdrop = {
+	bgFile = [[Interface\Tooltips\UI-Tooltip-Background]], tile = true, tileSize = 16,
+	edgeFile = [[Interface\Tooltips\UI-Tooltip-Border]], edgeSize = 16,
+	insets = { left = 5, right = 5, top = 5, bottom = 5 }
+}
 
 function lib.CreatePanel(parent, width, height)
 	local frame = CreateFrame("Frame", nil, parent)
@@ -20,8 +24,8 @@ function lib.CreatePanel(parent, width, height)
 	frame:SetFrameLevel(parent:GetFrameLevel() + 1)
 
 	frame:SetBackdrop(panelBackdrop)
-	frame:SetBackdropColor(0.1, 0.1, 0.1, 0.5)
-	frame:SetBackdropBorderColor(0.8, 0.8, 0.8, 0.5)
+	frame:SetBackdropColor(0.06, 0.06, 0.06, 0.4)
+	frame:SetBackdropBorderColor(0.6, 0.6, 0.6, 1)
 
 	frame:SetWidth(width or 1)
 	frame:SetHeight(height or 1)
