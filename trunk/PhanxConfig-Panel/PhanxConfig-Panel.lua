7bdc669d68866e72496918ee776587bf315b0cce
Akkorian-257529 <Akkorian-257529@5d13539c-25a5-4bc8-b52a-5474e3ee8778>
2011-02-18 04:32:07 +0000

    Added some basic error-checking

--- trunk/PhanxConfig-Panel/PhanxConfig-Panel.lua
+++ trunk/PhanxConfig-Panel/PhanxConfig-Panel.lua
@@ -19,6 +19,8 @@ local panelBackdrop = {
 }
 
 function lib.CreatePanel(parent, width, height)
+	assert( type(parent) == "table" and parent.GetFrameType, "PhanxConfig-Panel: Parent is not a valid frame!" )
+
 	local frame = CreateFrame("Frame", nil, parent)
 	frame:SetFrameStrata(parent:GetFrameStrata())
 	frame:SetFrameLevel(parent:GetFrameLevel() + 1)
