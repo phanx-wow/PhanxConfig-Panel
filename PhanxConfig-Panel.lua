0d475677e3521bbd0b0ad56b0921bdf8c48faca1
Phanx-28751 <Phanx-28751@5d13539c-25a5-4bc8-b52a-5474e3ee8778>
2014-02-07 22:29:53 +0000

    - Many consistent, all sides!

--- PhanxConfig-Panel.lua
+++ PhanxConfig-Panel.lua
@@ -7,7 +7,7 @@
 	its internals may change at any time without notice.
 ----------------------------------------------------------------------]]
 
-local MINOR_VERSION = tonumber(("$Revision$"):match("%d+"))
+local MINOR_VERSION = tonumber(strmatch("$Revision$", "%d+"))
 
 local lib, oldminor = LibStub:NewLibrary("PhanxConfig-Panel", MINOR_VERSION)
 if not lib then return end
@@ -18,19 +18,17 @@ local panelBackdrop = {
 	insets = { left = 5, right = 5, top = 5, bottom = 5 }
 }
 
-function lib.CreatePanel(parent, width, height)
-	assert( type(parent) == "table" and parent.CreateFontString, "PhanxConfig-Panel: Parent is not a valid frame!" )
+function lib:New(parent, width, height)
+	assert(type(parent) == "table" and type(rawget(parent, 0)) == "userdata", "PhanxConfig-Panel: Parent is not a valid frame!")
 
 	local frame = CreateFrame("Frame", nil, parent)
-	frame:SetFrameStrata(parent:GetFrameStrata())
-	frame:SetFrameLevel(parent:GetFrameLevel() + 1)
-
 	frame:SetBackdrop(panelBackdrop)
 	frame:SetBackdropColor(0.06, 0.06, 0.06, 0.4)
 	frame:SetBackdropBorderColor(0.6, 0.6, 0.6, 1)
 
-	frame:SetWidth(width or 1)
-	frame:SetHeight(height or 1)
+	frame:SetSize(width or 1, height or 1)
 
 	return frame
 end
+
+function lib.CreatePanel(...) return lib:New(...) end
